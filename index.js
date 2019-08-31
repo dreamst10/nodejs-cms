fbbf8db013c3623ae30085f15a2bfce316896782
Luis Vivas <dreamst923@gmail.com>
2019-08-30 23:10:11 -0400

    changed templating engine
    changed engine to express-handlebars

--- index.js
+++ index.js
@@ -2,16 +2,40 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const props=require('./utilities/properties');
+const exphbs=require('express-handlebars');
+const pg=require('pg');
+const logger=require('morgan');
+
+let session=require('express-session');
+let passport = require('passport');
+let cors = require('cors'); 
+
+
 
 //app.get('/', (req, res) => res.send('Hello World!'));
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended:false}));
 
 
-app.set('views','./views');
-app.set('view engine', 'pug');
 
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('views',path.join(__dirname,'views'));
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');;
+
+app.use(logger('dev'));
+  
 
 app.use('/',require('./routes/index'));
 
